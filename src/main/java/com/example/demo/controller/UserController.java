0fc3723005efd52a07437279fd807cd94ac329bc
murlov <amolovv@gmail.com>
2025-07-12 23:09:09 +0300

    finish

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -27,4 +27,16 @@ public class UserController {
         return userService.create(user);
     }
 
+    @DeleteMapping(path = "{id}")
+    public void delete(@PathVariable() Long id) {
+        userService.delete(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void update(
+            @PathVariable() Long id,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String name) {
+        userService.update(id, email, name);
+    }
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,8 +3,10 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(value = "select * from users where email = :email", nativeQuery = true)
-    public User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
